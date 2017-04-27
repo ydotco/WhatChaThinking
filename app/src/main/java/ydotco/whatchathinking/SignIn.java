fffd72b322237bcb6065d59870f4d4b11059c5d8
oFek <OBenzer@gmail.com>
2017-04-27 17:51:30 +0200

    Firebase SignIn/SignUp 1st Try

--- app/src/main/java/ydotco/whatchathinking/SignIn.java
+++ app/src/main/java/ydotco/whatchathinking/SignIn.java
@@ -1,14 +1,9 @@
 package ydotco.whatchathinking;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,23 +12,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import static android.R.attr.name;
-import static ydotco.whatchathinking.R.id.fab;
-
-public class SignUp extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    EditText username,mail,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_sign_in);
 
-        //Authentication Process//
+        //Firebase Authentication Part//
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -48,38 +37,40 @@ public class SignUp extends AppCompatActivity {
                 }
             }
         };
-
-        username= (EditText) findViewById(R.id.etName);
-        mail = (EditText) findViewById(R.id.etMail);
-        password = (EditText) findViewById(R.id.etPassword);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createAccount(password.getText().toString(),username.getText().toString()); //Create Account
-
-                //Forward the activity to signup hobbies//
-                Intent intent = new Intent(getApplicationContext(),SignUpHobbies.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    private void createAccount(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("SignUp New User", "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        Log.d("SignIn", "signInWithEmail:onComplete:" + task.isSuccessful());
 
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-                            Toast.makeText(SignUp.this, R.string.auth_failed,
+                            Log.w("SignIn", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(SignIn.this, R.string.auth_failed,
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
+
+
 }
